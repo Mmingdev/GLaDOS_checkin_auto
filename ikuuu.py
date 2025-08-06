8442aec69521b972c1563dcb39ecb1d874f447d7
Mmingdev <stormcar@dingtalk.com>
2025-08-06 20:51:15 +0800

    添加ikuuu签到

---
+++ ikuuu.py
@@ -0,0 +1,46 @@
+import requests,json,os
+# -------------------------------------------------------------------------------------------
+# github workflows
+# -------------------------------------------------------------------------------------------
+if __name__ == '__main__':
+# pushplus秘钥 申请地址 http://www.pushplus.plus/
+    sckey = os.environ.get("PUSHPLUS_TOKEN", "")
+# 推送内容
+    sendContent = ''
+# 账号cookie 直接使用数组 如果使用环境变量需要字符串分割一下
+    cookies = os.environ.get("ikuuu_COOKIE", []).split("&")
+    if cookies[0] == "":
+        print('未获取到COOKIE变量') 
+        cookies = []
+        exit(0)
+    url= "https://ikuuu.ch/user/checkin"
+    url2= "https://ikuuu.ch/user"
+    referer = 'https://ikuuu.ch/user'
+    origin = "https://ikuuu.ch"
+    useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36 Edg/136.0.0.0"
+    # payload={
+    #     'token': 'glados.one'
+    # }
+    for cookie in cookies:
+        checkin = requests.post(url,headers={'cookie': cookie ,'referer': referer,'origin':origin,'user-agent':useragent,'content-type':'text/html; charset=UTF-8','priority':'u=1, i'})#,data=json.dumps(payload)
+        #state =  requests.get(url2,headers={'cookie': cookie ,'referer': referer,'origin':origin,'user-agent':useragent})
+    #--------------------------------------------------------------------------------------------------------#  
+        #time = state.json()['data']['leftDays']
+    #     if isinstance(time,str):
+    #         time = time.split('.')[0]
+    #     else:
+    #         time=str(time)
+    #     email = state.json()['data']['email']
+    #     if 'message' in checkin.text:
+    #         mess = checkin.json()['message']
+    #         points = checkin.json()['list'][0]['balance'].split(".")[0]
+    #         print(email+'----结果--'+mess+'----剩余('+time+')天'+'----点数('+points+')')  # 日志输出
+    #         sendContent += mess+'--剩余('+time+')天'+'--点数('+points+')--'+email+'\n'
+    #     else:
+    #         requests.get('http://www.pushplus.plus/send?token=' + sckey + '&content='+email+'cookie已失效')
+    #         print('cookie已失效')  # 日志输出
+    #  #--------------------------------------------------------------------------------------------------------#
+    # if sckey != "":
+    #      requests.get('http://www.pushplus.plus/send?token=' + sckey + '&title='+email+'签到成功'+'&content='+sendContent)
+
+
